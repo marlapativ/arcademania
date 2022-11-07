01cc84a09bb9bd003df3b1ecb21f24b588af9207
Teja Marlapati <marlapati.v@northeastern.edu>
2022-11-07 14:58:34 -0500

    Swagger

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -5,15 +5,19 @@ import swaggerUI from 'swagger-ui-express';
 import swaggerDoc from './swagger.json';
 
 const routes = (app: Express) => {
+    // Signup Routes
+    app.use('/', authRouter);
+
     // Setting up swagger
     setupSwagger(app);
 
-    // Signup Routes
-    app.use('/', authRouter);
 }
 
 const setupSwagger = (app: Express) => {
     app.use('/swagger', swaggerUI.serve, swaggerUI.setup(swaggerDoc))
+    app.get('*', (req, res) => {
+        res.redirect('/swagger');
+    });
 }
 
 export default routes;
