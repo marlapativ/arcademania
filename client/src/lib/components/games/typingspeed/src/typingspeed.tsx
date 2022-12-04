f8a76f75575372d6bc81405474c051e201adca05
Teja Marlapati <marlapati.v@northeastern.edu>
2022-12-04 15:53:57 -0500

    Added function to calculate words per min

--- client/src/lib/components/games/typingspeed/src/typingspeed.tsx
+++ client/src/lib/components/games/typingspeed/src/typingspeed.tsx
@@ -21,5 +21,37 @@ class TypingSpeed extends Component {
     wordsPerMinute: null
   } 
 
+componentDidMount() {
+    this.setState({words: this.state.typeTest.split(' ')})
+  }
 
+  wordsPerMinute = (charsTyped: number, millis: number): number =>
+    Math.floor((charsTyped / 5) / (millis / 60000))
+
+  checkFinished = (): void => {
+    if (!this.state.words.length) {
+      if (this.state.startTime) {
+        const timeMillis: number = new Date().getTime() - this.state.startTime.getTime()
+        const wpm = this.wordsPerMinute(this.state.typeTest.length, timeMillis)
+        this.setState({wordsPerMinute: wpm})
+      }
+    }
+  }
+
+  onWordChange = (e: ChangeEvent<HTMLInputElement>): void => {
+    if (!this.state.started) {
+      this.setState({started: true, startTime: new Date()})  
+    }
+
+    console.log(e.currentTarget.value)
+    const enteredText = e.currentTarget.value.trim()
+    this.setState({enteredText})
+    if (enteredText === this.state.words[0]) {
+      console.log('Right')
+      this.setState({correctCount: this.state.correctCount + 1})
+      this.setState({enteredText: ''})
+      this.setState({words: this.state.words.slice(1)},
+                    (): void => this.checkFinished())
+    }
+  }
 export default TypingSpeed
