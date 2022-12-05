db17febbf22929f8b5ecef60bbc4cd987515832c
Teja Marlapati <marlapati.v@northeastern.edu>
2022-12-04 19:18:37 -0500

    Adding styles

--- client/src/lib/components/games/memorygame/src/components/Card/Card.styles.ts
+++ client/src/lib/components/games/memorygame/src/components/Card/Card.styles.ts
@@ -9,3 +9,16 @@ export const Wrapper = styled.div`
     transform: rotateY(180deg);
   }
 `;
+
+type Props = {
+  flipped: boolean;
+};
+
+const sharedStyles = css`
+  width: 100%;
+  height: 100%;
+  transition: all 0.5s;
+  backface-visibility: hidden;
+  cursor: pointer;
+  transform-style: preserve-3d;
+`;
