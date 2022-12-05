28faa1a63db7710cf4d00dc5370c221a0e95bdd3
Teja Marlapati <marlapati.v@northeastern.edu>
2022-12-04 19:44:40 -0500

    Updated game to index file

--- client/src/lib/components/games/index.tsx
+++ client/src/lib/components/games/index.tsx
@@ -4,5 +4,6 @@ import Minesweeper from "./minesweeper/Minesweeper";
 import SnakeGame from "./snakegame/SnakeGame";
 import TicTacToe from "./tictactoe/src/tictactoe";
 import TypingSpeed from "./typingspeed/src/typingspeed";
+import MemoryGame from './memorygame/src/MemoryGame';
 
-export { Minesweeper, SnakeGame, CarGame, Connect4, TicTacToe, TypingSpeed };
+export { Minesweeper, SnakeGame, CarGame, Connect4, TicTacToe, TypingSpeed, MemoryGame };
