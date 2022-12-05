21328c7261ac071b1031d0a446f73f047245e5e5
Teja Marlapati <marlapati.v@northeastern.edu>
2022-12-04 19:07:14 -0500

    Importing images from public

---
+++ client/src/lib/components/games/memorygame/src/setup.ts
@@ -0,0 +1,20 @@
+import card1 from '../../../../../../public/img/card_1.jpg';
+import card2 from '../../../../../../public/img/card_2.jpg';
+import card3 from '../../../../../../public/img/card_3.jpg';
+import card4 from '../../../../../../public/img/card_4.jpg';
+import card5 from '../../../../../../public/img/card_5.jpg';
+import card6 from '../../../../../../public/img/card_6.jpg';
+import card7 from '../../../../../../public/img/card_7.jpg';
+import card8 from '../../../../../../public/img/card_8.jpg';
+// Cardback
+import cardBack from '../../../../../../public/img/card_back.jpg';
+
+export type CardType = {
+  id: string;
+  flipped: boolean;
+  backImage: string;
+  frontImage: string;
+  clickable: boolean;
+  matchingCardId: string;
+};
+
