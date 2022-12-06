ee1630d4cc9e897c8e73a51a871e6daa59067bbb
Teja Marlapati <marlapati.v@northeastern.edu>
2022-12-06 02:01:52 -0500

    added tictactoe.types

---
+++ client/src/lib/types/components/games/tictactoe.types.ts
@@ -0,0 +1,15 @@
+export type SquareType = "X" | "O" | null;
+
+export interface SquareProps {
+  value: SquareType;
+  onClick(): void;
+}
+
+export interface BoardProps {
+  squares: SquareType[];
+  onClick(i: number): void;
+}
+
+export interface History {
+  squares: SquareType[];
+}
