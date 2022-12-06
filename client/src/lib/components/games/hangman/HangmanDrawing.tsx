1e4271fc60969b1c0ff5398fac5e79e4b341b6c0
Teja Marlapati <marlapati.v@northeastern.edu>
2022-12-05 19:20:14 -0500

    styles for arms

--- client/src/lib/components/games/hangman/HangmanDrawing.tsx
+++ client/src/lib/components/games/hangman/HangmanDrawing.tsx
@@ -24,3 +24,33 @@ const BODY = (
     }}
   />
 )
+
+const RIGHT_ARM = (
+  <div
+    style={{
+      width: "100px",
+      height: "10px",
+      background: "black",
+      position: "absolute",
+      top: "150px",
+      right: "-100px",
+      rotate: "-30deg",
+      transformOrigin: "left bottom",
+    }}
+  />
+)
+
+const LEFT_ARM = (
+  <div
+    style={{
+      width: "100px",
+      height: "10px",
+      background: "black",
+      position: "absolute",
+      top: "150px",
+      right: "10px",
+      rotate: "30deg",
+      transformOrigin: "right bottom",
+    }}
+  />
+)
