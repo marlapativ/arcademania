1f8df6d6dba3c52fc4477ddb8c9ad6c836f7f616
Teja Marlapati <marlapati.v@northeastern.edu>
2022-12-05 19:17:31 -0500

    return function added

--- client/src/lib/components/games/hangman/Hangman.tsx
+++ client/src/lib/components/games/hangman/Hangman.tsx
@@ -1,5 +1,6 @@
 import { useCallback, useEffect, useState } from "react"
 
+
 function getWord() {
   return words[Math.floor(Math.random() * words.length)]
 }
@@ -25,7 +26,6 @@ function Hangman() {
     },
     [guessedLetters, isWinner, isLoser]
   )
-
 
   useEffect(() => {
     const handler = (e: KeyboardEvent) => {
@@ -60,6 +60,39 @@ function Hangman() {
     }
   }, [])
 
-  }
+  return (
+    <div
+      style={{
+        maxWidth: "800px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "2rem",
+        margin: "0 auto",
+        alignItems: "center",
+      }}
+    >
+      <div style={{ fontSize: "2rem", textAlign: "center" }}>
+        {isWinner && "Winner! - Refresh to try again"}
+        {isLoser && "Nice Try - Refresh to try again"}
+      </div>
+      <HangmanDrawing numberOfGuesses={incorrectLetters.length} />
+      <HangmanWord
+        reveal={isLoser}
+        guessedLetters={guessedLetters}
+        wordToGuess={wordToGuess}
+      />
+      <div style={{ alignSelf: "stretch" }}>
+        <Keyboard
+          disabled={isWinner || isLoser}
+          activeLetters={guessedLetters.filter(letter =>
+            wordToGuess.includes(letter)
+          )}
+          inactiveLetters={incorrectLetters}
+          addGuessedLetter={addGuessedLetter}
+        />
+      </div>
+    </div>
+  )
+}
 
 export default Hangman
