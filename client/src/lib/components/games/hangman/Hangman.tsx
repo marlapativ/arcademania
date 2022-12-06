10dd59a3bd93110565c153819b8a6c587fb51ca9
Teja Marlapati <marlapati.v@northeastern.edu>
2022-12-05 19:53:51 -0500

    Added imports

--- client/src/lib/components/games/hangman/Hangman.tsx
+++ client/src/lib/components/games/hangman/Hangman.tsx
@@ -1,5 +1,8 @@
 import { useCallback, useEffect, useState } from "react"
-
+import { HangmanDrawing } from "./HangmanDrawing"
+import { HangmanWord } from "./HangmanWord"
+import { Keyboard } from "./Keyboard"
+import words from "./wordList.json"
 
 function getWord() {
   return words[Math.floor(Math.random() * words.length)]
