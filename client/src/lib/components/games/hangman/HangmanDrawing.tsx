793a49097f4aadbb17ac2162923bc655a910c7cc
Teja Marlapati <marlapati.v@northeastern.edu>
2022-12-05 19:19:47 -0500

    styles for head and body

---
+++ client/src/lib/components/games/hangman/HangmanDrawing.tsx
@@ -0,0 +1,26 @@
+const HEAD = (
+  <div
+    style={{
+      width: "50px",
+      height: "50px",
+      borderRadius: "100%",
+      border: "10px solid black",
+      position: "absolute",
+      top: "50px",
+      right: "-30px",
+    }}
+  />
+)
+
+const BODY = (
+  <div
+    style={{
+      width: "10px",
+      height: "100px",
+      background: "black",
+      position: "absolute",
+      top: "120px",
+      right: 0,
+    }}
+  />
+)
