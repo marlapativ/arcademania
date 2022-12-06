cebbe698781190d6acf6912ba816f53002957169
Teja Marlapati <marlapati.v@northeastern.edu>
2022-12-06 18:10:52 -0500

    Minesweeper help

--- client/src/lib/components/games/index.tsx
+++ client/src/lib/components/games/index.tsx
@@ -77,7 +77,8 @@ const games: GameInfoCollection = {
     altImage: "images/Minesweeper.png",
     description:
       "Classic implementation of the Minesweeper game in Javascript with flag the cell feature.",
-    helpContent: "",
+    helpContent:
+      "Minesweeper is a game where mines are hidden in a grid of squares. Safe squares have numbers telling you how many mines touch the square. You can use the number clues to solve the game by opening all of the safe squares. If you click on a mine you lose the game!",
     icon: FaBomb,
   },
   6: {
