704265a047cb5ff1811d608456200e7f4df8c18d
Teja Marlapati <marlapati.v@northeastern.edu>
2022-12-05 19:16:01 -0500

    Adding source code for Hangman

---
+++ client/src/lib/components/games/hangman/Hangman.tsx
@@ -0,0 +1,31 @@
+import { useCallback, useEffect, useState } from "react"
+
+function getWord() {
+  return words[Math.floor(Math.random() * words.length)]
+}
+
+function Hangman() {
+  const [wordToGuess, setWordToGuess] = useState(getWord)
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
+
+  const incorrectLetters = guessedLetters.filter(
+    letter => !wordToGuess.includes(letter)
+  )
+
+  const isLoser = incorrectLetters.length >= 6
+  const isWinner = wordToGuess
+    .split("")
+    .every(letter => guessedLetters.includes(letter))
+
+  const addGuessedLetter = useCallback(
+    (letter: string) => {
+      if (guessedLetters.includes(letter) || isLoser || isWinner) return
+
+      setGuessedLetters(currentLetters => [...currentLetters, letter])
+    },
+    [guessedLetters, isWinner, isLoser]
+  )
+
+  }
+
+export default Hangman
