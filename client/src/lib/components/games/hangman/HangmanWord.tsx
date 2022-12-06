f873546b7d1843aa6cd62122c575828d25b901e4
Teja Marlapati <marlapati.v@northeastern.edu>
2022-12-05 19:22:31 -0500

    updating function

--- client/src/lib/components/games/hangman/HangmanWord.tsx
+++ client/src/lib/components/games/hangman/HangmanWord.tsx
@@ -23,7 +23,11 @@ export function HangmanWord({
       {wordToGuess.split("").map((letter, index) => (
         <span style={{ borderBottom: ".1em solid black" }} key={index}>
           <span
-           
+            style={{
+              visibility:
+                guessedLetters.includes(letter) || reveal
+                  ? "visible"
+                  : "hidden",
               color:
                 !guessedLetters.includes(letter) && reveal ? "red" : "black",
             }}
