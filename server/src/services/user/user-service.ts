482d43024b6890713ec4bd5b41a51000abd433cb
Teja Marlapati <marlapati.v@northeastern.edu>
2022-12-06 21:05:03 -0500

    user update

--- server/src/services/user/user-service.ts
+++ server/src/services/user/user-service.ts
@@ -2,8 +2,9 @@ import mongoose from "mongoose";
 import { User } from "../../models";
 import { IUser } from "../../types/models/user.types";
 
-export const updateUser = async (id:number, user: IUser) => {
+export const updateUser = async (id:mongoose.ObjectId, user: IUser) => {
     const updatedUser =  await User.findByIdAndUpdate(id, user);
+    updatedUser.save();
     if (!updatedUser) {
       throw new Error("User Update failed ");
     }
