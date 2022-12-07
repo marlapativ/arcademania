6a6fe55e55060f7225575185b0c9ed5362558554
Teja Marlapati <marlapati.v@northeastern.edu>
2022-12-06 19:55:03 -0500

    Help for TicTacToe

--- client/src/lib/components/games/index.tsx
+++ client/src/lib/components/games/index.tsx
@@ -85,10 +85,10 @@ const games: GameInfoCollection = {
     id: 6,
     name: "Tic Tac Toe",
     component: <TicTacToe />,
-    helpContent: "",
     description:
       "Two players take turns in drawing either an ` O' or an ` X' in one square of a grid consisting of nine squares.",
-    icon: GiTicTacToe,
+    helpContent: "Two players take turns in drawing either an ` O' or an ` X' in one square of a grid consisting of nine squares.",
+      icon: GiTicTacToe,
   },
   7: {
     id: 7,
