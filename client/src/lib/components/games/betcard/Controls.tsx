ff17cc904d7c9a813b059e56ad9b4a38c566e178
Teja Marlapati <marlapati.v@northeastern.edu>
2022-12-07 18:36:56 -0500

    adding controls functionality

--- client/src/lib/components/games/betcard/Controls.tsx
+++ client/src/lib/components/games/betcard/Controls.tsx
@@ -11,49 +11,65 @@ type ControlsProps = {
   resetEvent: any
 };
 
-const validation = () => {
-  if (amount > balance) {
-    setInputStyle(styles.inputError);
-    return false;
-  }
-  if (amount < 0.01) {
-    setInputStyle(styles.inputError);
-    return false;
-  }
-  setInputStyle(styles.input);
-  return true;
-}
+const Controls: React.FC<ControlsProps> = ({ balance, gameState, buttonState, betEvent, hitEvent, standEvent, resetEvent }) => {
+  const [amount, setAmount] = useState(10);
+  const [inputStyle, setInputStyle] = useState(styles.input);
 
-const amountChange = (e: any) => {
-  setAmount(e.target.value);
-}
+  useEffect(() => {
+    validation();
+  }, [amount, balance]);
 
-const onBetClick = () => {
-  if (validation()) {
-    betEvent(Math.round(amount * 100) / 100);
+  const validation = () => {
+    if (amount > balance) {
+      setInputStyle(styles.inputError);
+      return false;
+    }
+    if (amount < 0.01) {
+      setInputStyle(styles.inputError);
+      return false;
+    }
+    setInputStyle(styles.input);
+    return true;
   }
-}
 
-const getControls = () => {
-  if (gameState === 0) {
-    return (
-      <div className={styles.controlsContainer}>
-        <div className={styles.betContainer}>
-          <h4>Amount:</h4>
-          <input autoFocus type='number' value={amount} onChange={amountChange} className={inputStyle} />
+  const amountChange = (e: any) => {
+    setAmount(e.target.value);
+  }
+
+  const onBetClick = () => {
+    if (validation()) {
+      betEvent(Math.round(amount * 100) / 100);
+    }
+  }
+
+  const getControls = () => {
+    if (gameState === 0) {
+      return (
+        <div className={styles.controlsContainer}>
+          <div className={styles.betContainer}>
+            <h4>Amount:</h4>
+            <input autoFocus type='number' value={amount} onChange={amountChange} className={inputStyle} />
+          </div>
+          <button onClick={() => onBetClick()} className={styles.button}>Bet</button>
         </div>
-        <button onClick={() => onBetClick()} className={styles.button}>Bet</button>
-      </div>
-    );
+      );
+    }
+    else {
+      return (
+        <div className={styles.controlsContainer}>
+          <button onClick={() => hitEvent()} disabled={buttonState.hitDisabled} className={styles.button}>Hit</button>
+          <button onClick={() => standEvent()} disabled={buttonState.standDisabled} className={styles.button}>Stand</button>
+          <button onClick={() => resetEvent()} disabled={buttonState.resetDisabled} className={styles.button}>Reset</button>
+        </div>
+      );
+    }
   }
-  else {
-    return (
-      <div className={styles.controlsContainer}>
-        <button onClick={() => hitEvent()} disabled={buttonState.hitDisabled} className={styles.button}>Hit</button>
-        <button onClick={() => standEvent()} disabled={buttonState.standDisabled} className={styles.button}>Stand</button>
-        <button onClick={() => resetEvent()} disabled={buttonState.resetDisabled} className={styles.button}>Reset</button>
-      </div>
-    );
-  }
+
+  return (
+    <>
+      {getControls()}
+    </>
+  );
 }
 
+export default Controls;
