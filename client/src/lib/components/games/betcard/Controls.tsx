7c2e596b562e43f15b773a5af63c6995e25a6d89
Teja Marlapati <marlapati.v@northeastern.edu>
2022-12-07 18:36:21 -0500

    adding controls functionality

--- client/src/lib/components/games/betcard/Controls.tsx
+++ client/src/lib/components/games/betcard/Controls.tsx
@@ -11,3 +11,49 @@ type ControlsProps = {
   resetEvent: any
 };
 
+const validation = () => {
+  if (amount > balance) {
+    setInputStyle(styles.inputError);
+    return false;
+  }
+  if (amount < 0.01) {
+    setInputStyle(styles.inputError);
+    return false;
+  }
+  setInputStyle(styles.input);
+  return true;
+}
+
+const amountChange = (e: any) => {
+  setAmount(e.target.value);
+}
+
+const onBetClick = () => {
+  if (validation()) {
+    betEvent(Math.round(amount * 100) / 100);
+  }
+}
+
+const getControls = () => {
+  if (gameState === 0) {
+    return (
+      <div className={styles.controlsContainer}>
+        <div className={styles.betContainer}>
+          <h4>Amount:</h4>
+          <input autoFocus type='number' value={amount} onChange={amountChange} className={inputStyle} />
+        </div>
+        <button onClick={() => onBetClick()} className={styles.button}>Bet</button>
+      </div>
+    );
+  }
+  else {
+    return (
+      <div className={styles.controlsContainer}>
+        <button onClick={() => hitEvent()} disabled={buttonState.hitDisabled} className={styles.button}>Hit</button>
+        <button onClick={() => standEvent()} disabled={buttonState.standDisabled} className={styles.button}>Stand</button>
+        <button onClick={() => resetEvent()} disabled={buttonState.resetDisabled} className={styles.button}>Reset</button>
+      </div>
+    );
+  }
+}
+
