d898b2562f0f0fc30795f419b104ac4c9b5b0466
Teja Marlapati <marlapati.v@northeastern.edu>
2022-12-07 17:49:17 -0500

    Adding documentation

--- client/src/lib/components/games/memorygame/Card/Card.tsx
+++ client/src/lib/components/games/memorygame/Card/Card.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 // Types
 import { CardType } from '../setup';
-// Styles
+// Styles to be used
 import { Wrapper, FrontImg, BackImg } from './Card.styles';
-
+//using props
 type Props = {
   card: CardType;
   callback: (card: CardType) => void;
 };
-
+//checking clicks with condition
 const Card: React.FC<Props> = ({ card, callback }) => {
   const handleClick = () => {
     if (card.clickable) callback(card);
   };
-
+//wrapper to br returned here
   return (
     <Wrapper onClick={handleClick}>
       <FrontImg flipped={card.flipped} src={card.frontImage} alt='card-front' />
