c5ff1bc417b901c6653d3a093119c79e24aca150
Teja Marlapati <marlapati.v@northeastern.edu>
2022-12-06 22:34:15 -0500

    documentation

--- server/src/services/user/user-service.ts
+++ server/src/services/user/user-service.ts
@@ -2,6 +2,11 @@ import mongoose from "mongoose";
 import { User } from "../../models";
 import { IUser } from "../../types/models/user.types";
 
+/**
+ * This method finds the user with the help of id and updates it with the given details
+ * @param id - id value of type mongoose.ObjectId
+ * @param user - user object of type IUser with the details of users that needs to be updated 
+ */
 export const updateUser = async (id:mongoose.ObjectId, user: IUser) => {
     const updatedUser =  await User.findByIdAndUpdate(id, user);
     updatedUser.save();
@@ -11,6 +16,10 @@ export const updateUser = async (id:mongoose.ObjectId, user: IUser) => {
     return updatedUser;
   }
 
+/**
+ * This method returns the user identified with the help of userId
+ * @param userId - id value of type mongoose.ObjectId
+ */
   export const getUser = async (userId:mongoose.ObjectId) => {
     const user =  await User.findById(userId);
     if (!user) {
